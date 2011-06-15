9e5a5d7f16e66832024678deedc7514a62ebc33f
Yasuharu NAKANO <ynak@jggug.org>
2011-06-15 10:57:14 +0900

    added a sample of replacement of declaration

--- src/main/groovy/org/jggug/kobo/gastah/ForLearning.groovy
+++ src/main/groovy/org/jggug/kobo/gastah/ForLearning.groovy
@@ -27,6 +27,8 @@ class ForLearningASTTransformation extends ClassCodeVisitorSupport implements AS
         AnnotatedNode target = (AnnotatedNode) nodes[1]
         AnnotationNode includeAnnotation = (AnnotationNode) nodes[0]
 
+        dump(nodes, source)
+
         //def additionNode = new AstBuilder().buildFromCode({
         //    // 明示的なreturnが内場合には、暗黙のreturn(ReturnBlock)になってしまう
         //    // →既存コードにステートメント追加しても後続の処理が継続しない
@@ -41,10 +43,11 @@ class ForLearningASTTransformation extends ClassCodeVisitorSupport implements AS
             println "ZZZ!"
             return null // 明示的returnがあれば、それ以前のステートメントがReturnBlockになることはない
         })[0]
-        def statements = additionNode.statements // 全て追加する場合(ReturnBlock含む)
-        // 最後のReturnBlockは使わない場合
-        if (statements.size() < 2) return
-        statements = statements[0..-2]
+        def additionStatements = additionNode.statements
+        if (additionStatements.size() >= 2) { // 最後のReturnBlockは使わない場合
+            additionStatements = additionStatements[0..-2]
+        }
+        def additionStatement = new BlockStatement(additionStatements, new VariableScope())
 
         //def additionVariable = new AstBuilder().buildFromSpec({
         //    declaration {
@@ -55,15 +58,47 @@ class ForLearningASTTransformation extends ClassCodeVisitorSupport implements AS
         //})
 
         // ブロック先頭への追加
-        source.getAST().getStatementBlock().getStatements().addAll(0, statements)
+        //source.getAST().getStatementBlock().getStatements().addAll(0, additionStatement)
 
         // ブロック末尾への追加
-        //source.getAST().getStatementBlock().getStatements().addAll(statements)
+        //source.getAST().getStatementBlock().getStatements().addAll(additionStatement)
 
         // ブロック内の完全リプレース
         //source.getAST().getStatementBlock().getStatements().clear()
-        //source.getAST().getStatementBlock().getStatements().addAll(0, statements)
+        //source.getAST().getStatementBlock().getStatements().addAll(0, additionStatement)
+
+        // アノテーションを付けた文のみ置換
+        def astStatements = source.getAST().statementBlock.statements
+        source.getAST().statementBlock.statements = astStatements.collect { stmt ->
+            if (stmt in ExpressionStatement && stmt.expression.is(target)) {
+                return additionStatement
+            }
+            return stmt
+        }
     }
 
+    void dump(nodes, source) {
+        def target = (AnnotatedNode) nodes[1]
+        def includeAnnotation = (AnnotationNode) nodes[0]
+        def printWithIndent = { println " "*4 + it }
+        println ">"*50
+        println "Nodes: ["
+        nodes.each printWithIndent
+        println "]"
+        println "Target(Nodes[1]): $target"
+        println "Annotation(Nodes[0]): ["
+        includeAnnotation.properties.each printWithIndent
+        println "]"
+        println "SourceUnit: ["
+        source.properties.each printWithIndent
+        println "]"
+        println "<"*50
+    }
+
+    void dump(Statement stmt) {
+        println "${stmt.class.simpleName}: ["
+        stmt.properties.each printWithIndent
+        println "]"
+    }
 }
 
