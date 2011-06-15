1c6a30fe80b8e5752da854e65481f000bbf02c65
Yasuharu NAKANO <ynak@jggug.org>
2011-06-15 10:01:15 +0900

    added a script using @ToFile

---
+++ script/tofile.groovy
@@ -0,0 +1,10 @@
+import org.jggug.kobo.gastah.*
+
+@ToFile("/tmp/hoge.txt")
+class X { }
+
+new X().println("Hello! 1")
+new X().xprintln("Hello! 1")
+new X().xprintln("Hello! 2")
+new X().xprintln("Hello! 3")
+
