c6741deeb041130717ada22271bc10560d12f461
Yasuharu NAKANO <ynak@jggug.org>
2011-06-15 09:45:50 +0900

    unified a file of annotation and ASTTransformation for easy management

--- src/main/groovy/org/jggug/kobo/gastah/ToFile.groovy
+++ src/main/groovy/org/jggug/kobo/gastah/ToFile.groovy
@@ -1,7 +1,14 @@
 package org.jggug.kobo.gastah
 
-import org.codehaus.groovy.transform.GroovyASTTransformationClass
 import java.lang.annotation.*
+import org.codehaus.groovy.ast.*
+import org.codehaus.groovy.ast.expr.*
+import org.codehaus.groovy.ast.stmt.*
+import org.codehaus.groovy.ast.builder.*
+import org.codehaus.groovy.control.*
+import org.codehaus.groovy.transform.*
+import org.objectweb.asm.Opcodes
+import static org.objectweb.asm.Opcodes.*
 
 @GroovyASTTransformationClass("org.jggug.kobo.gastah.ToFileASTTransformation")
 @Retention(RetentionPolicy.RUNTIME)
@@ -9,4 +16,39 @@ import java.lang.annotation.*
 public @interface ToFile {
 
     String value()
+}
+
+@GroovyASTTransformation(phase = CompilePhase.CANONICALIZATION)
+class ToFileASTTransformation extends ClassCodeVisitorSupport implements ASTTransformation {
+
+    SourceUnit sourceUnit   // forced to override by super class
+    final static Set<Class> visitedClass = new HashSet<Class>().asSynchronized()
+
+    public void visit(ASTNode[] nodes, SourceUnit source) {
+        AnnotatedNode targetClass = (AnnotatedNode) nodes[1]
+        AnnotationNode includeAnnotation = (AnnotationNode) nodes[0]
+
+        if (visitedClass.contains(targetClass)) return
+
+        targetClass.addMethod(new AstBuilder().buildFromSpec {
+            method('xprintln', Opcodes.ACC_PUBLIC, Void.class) {
+                parameters {
+                    parameter 'arg': Object.class
+                }
+                exceptions {}
+                block {
+                    expression.addAll(new AstBuilder().buildFromCode({
+                        if (arg != null) {
+                            def file = this.class.getAnnotation(org.jggug.kobo.gastah.ToFile.class).value()
+                            new File(file).append(arg + System.properties["line.separator"])
+                        }
+                    }))
+                }
+                annotations {}
+            }
+        }[0])
+
+        visitedClass.add(targetClass)
+    }
+
 }
